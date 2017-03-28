3040097b2b44cd1b2f7847e19eccb51ae46fdb6c
aimengaba <aimengaba@gmail.com>
2017-03-28 22:56:35 +0500

    signups

--- js/signup.js
+++ js/signup.js
@@ -1,53 +1,43 @@
-//Problem: Hints are shown even when form is valid
-//Solution: Hide and show them at appropriate times
-var $password = $("#password");
-var $confirmPassword = $("#confirm_password");
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
 
-//Hide hints
-$("form span").hide();
+  var $this = $(this),
+      label = $this.prev('label');
 
-function isPasswordValid() {
-  return $password.val().length > 8;
-}
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight');
+			} else {
+		    label.removeClass('highlight');
+			}
+    } else if (e.type === 'focus') {
 
-function arePasswordsMatching() {
-  return $password.val() === $confirmPassword.val();
-}
-
-function canSubmit() {
-  return isPasswordValid() && arePasswordsMatching();
-}
-
-function passwordEvent(){
-    //Find out if password is valid
-    if(isPasswordValid()) {
-      //Hide hint if valid
-      $password.next().hide();
-    } else {
-      //else show hint
-      $password.next().show();
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight');
+			}
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
     }
-}
 
-function confirmPasswordEvent() {
-  //Find out if password and confirmation match
-  if(arePasswordsMatching()) {
-    //Hide hint if match
-    $confirmPassword.next().hide();
-  } else {
-    //else show hint
-    $confirmPassword.next().show();
-  }
-}
+});
 
-function enableSubmitEvent() {
-  $("#submit").prop("disabled", !canSubmit());
-}
+$('.tab a').on('click', function (e) {
 
-//When event happens on password input
-$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+  e.preventDefault();
 
-//When event happens on confirmation input
-$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
 
-enableSubmitEvent();
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
+});
